4d9ed005fecd598dae4d52b7ebf3c7efe6b10ee6
Ted Zhang <jony.s.thomas@gmail.com>
2018-10-07 09:42:15 -0400

    Initial Commit

---
+++ Restaurants/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Restaurants
+//
+//  Created by Ted Zhang on 10/7/18.
+//  Copyright © 2018 Ted Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Restaurants/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Restaurants
+//
+//  Created by Ted Zhang on 10/7/18.
+//  Copyright © 2018 Ted Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
